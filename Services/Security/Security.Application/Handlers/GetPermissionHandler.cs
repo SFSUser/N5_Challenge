9083f2d3c99e66876f004cef3bf9aa0951eed6b4
SFSUser <sfstricks@hotmail.com>
2022-05-21 16:13:26 -0500

    xUnit tests

--- Services/Security/Security.Application/Handlers/GetPermissionHandler.cs
+++ Services/Security/Security.Application/Handlers/GetPermissionHandler.cs
@@ -2,6 +2,7 @@ using MediatR;
 using Security.Application.Contracts.Persistence;
 using Security.Application.Queries;
 using Security.Core.Entities;
+using Security.Core.Repositories.Query;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -11,14 +12,21 @@ namespace Security.Application.Handlers.QueryHandlers
     public class GetPermissionHandler : IRequestHandler<GetPermissionQuery, Permissions>
     {
         private readonly IUnitOfWork _mediator;
+        private readonly IPermissionsQueryRepository _repoQuery;
 
-        public GetPermissionHandler(IUnitOfWork mediator)
+        /*public GetPermissionHandler(IUnitOfWork mediator)
         {
             _mediator = mediator;
+        }*/
+
+        public GetPermissionHandler(IPermissionsQueryRepository mediator)
+        {
+            _repoQuery = mediator;
         }
+
         public async Task<Permissions> Handle(GetPermissionQuery request, CancellationToken cancellationToken)
         {
-            var selectedPermission = await _mediator.PermissionsQueryRepository.GetPermissionAsync(request.Id);
+            var selectedPermission = await _repoQuery.GetPermissionAsync(request.Id);
             return selectedPermission;
         }
     }
---
+++ Services/Security/Test/Handlers/GetPermissionHandlerTests.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using Moq;
+using Security.Application.Handlers.CommandHandler;
+using Security.Application.Handlers.QueryHandlers;
+using Security.Application.Mapper;
+using Security.Application.Queries;
+using Security.Core.Entities;
+using Security.Core.Repositories.Query;
+using Security.Test.Mocks;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Security.Test.Handlers
+{
+    public class GetPermissionHandlerTests
+    {
+        private readonly IMapper _mapper;
+        private readonly Mock<IPermissionsQueryRepository> _mockRepo;
+        public GetPermissionHandlerTests()
+        {
+            _mockRepo = MockPermissionsRepository.PermissionsQueryRepository();
+
+            var mapperConfig = new MapperConfiguration(c => 
+            {
+                c.AddProfile<SecurityMappingProfile>();
+            });
+
+            _mapper = mapperConfig.CreateMapper();
+        }
+
+        [Fact]
+        public async Task GetPermission()
+        {
+            var handler = new GetPermissionHandler(_mockRepo.Object);
+
+            var result = await handler.Handle(new GetPermissionQuery(1), CancellationToken.None);
+
+            result.ShouldBeOfType<Permissions>();
+
+            result.Id.ShouldBe(1);
+        }
+    }
+}
